934857a69204bd200b17fa9197d2e9ab8727ecbc
luminest <drunkengorila@gmail.com>
2021-04-20 18:54:37 +0900

    sorry:(

--- genSnipplet.java
+++ genSnipplet.java
@@ -1,6 +1,8 @@
 package woohuij;
 
-public class Snipplet {
+//부끄럽지만 JAVA사용법을 익히지 못했습니다...
+
+public class genSnipplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
